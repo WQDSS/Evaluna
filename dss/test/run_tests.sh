6f41c9a45a171262cc9badf6b7fe010850d79d5c
Booh <ben.dayan@gmail.com>
2019-10-19 23:09:44 +0300

    travis-ci is failing, testing with larger sleep

--- dss/test/run_tests.sh
+++ dss/test/run_tests.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # ignore collections warning about deprecation warning, there's nothing we can do about that for now
-sleep 10 && pytest -W ignore::DeprecationWarning "$@"
+sleep 20 && pytest -W ignore::DeprecationWarning "$@"
