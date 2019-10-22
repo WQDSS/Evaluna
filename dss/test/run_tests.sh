55d5cfa37c3310b46f002b6af7c911271cbd956c
Booh <ben.dayan@gmail.com>
2019-10-22 08:53:41 +0300

    Increasing sleep for testing, no easy way to check for readiness in docker-compose

--- dss/test/run_tests.sh
+++ dss/test/run_tests.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # ignore collections warning about deprecation warning, there's nothing we can do about that for now
-sleep 20 && pytest -W ignore::DeprecationWarning "$@"
+sleep 40 && pytest -W ignore::DeprecationWarning "$@"
