6ff1e27b3a8f80efdb8b6c2765d3583059c77f02
Booh <ben.dayan@gmail.com>
2019-11-02 00:05:00 +0200

    Added helm chart, basic sanity works

--- dss/scripts/build_and_push_to_docker_hub.sh
+++ dss/scripts/build_and_push_to_docker_hub.sh
@@ -1,7 +1,21 @@
 #! /bin/bash -e
+bump_type=""
+if [ "$1" == "bump" ] ; then
+    bump_type="patch"
+    if [ -n "$2" ] ; then
+        bump_type="$2"
+    fi
+fi
 
 current_commit=$(git rev-parse --short HEAD)
 image_tag=${TAG:-$current_commit}
 docker build --rm -f "Dockerfile" -t waterqualitydss:latest -t "booooh/waterqualitydss:$image_tag" .
 docker push "booooh/waterqualitydss:$image_tag"
+
+# update Chart.yaml
+if  [ -n "$bump_type" ] ; then
+
+    # update the appVersion in the chart, and the chart version (by default, a patch-level bump)
+    python dss/scripts/update_chart_version.py --bump_part "$bump_type" --app_version "${image_tag}"
+fi
 
