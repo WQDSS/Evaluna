96ece4940ccc37b52eccf5ab1c8da78124959fb8
Booh <ben.dayan@gmail.com>
2019-11-02 16:04:20 +0200

    Testing travis-ci updates

--- dss/scripts/build_and_run_tests.sh
+++ dss/scripts/build_and_run_tests.sh
@@ -6,33 +6,5 @@ export TAG
 # build the docker images, including the test image, and bump the appVersion in the chart
 ./dss/scripts/build_and_push_to_docker_hub.sh bump
 
-# create a namespace for the test
-test_ns=wq2dss-test-${TAG}
-kubectl create ns ${test_ns}
-
-# deploy the helm chart with the testing flag set
-helm install wq2dss dss/chart/wq2dss/ --namespace ${test_ns} --set test.enabled=true
-
-# wait for everything to come up (no init container and readiness checks in place yet)
-# TODO: remove the sleep when waiting for all components will work
-sleep 30
-
-# hack for bash in windows
-if [ -n "$MSYSTEM" ] ; then
-    MSYS2_ARG_CONV_EXCL="/test"
-    echo "setting an exception for /test renaming"
-    export MSYS2_ARG_CONV_EXCL
-fi
-
-ret=0
-kubectl --namespace ${test_ns} exec -it wq2dss-test /test/run_tests.sh /test/ || ret=$?
-echo "Return value for test was $ret"
-
-# if test was successfull - clean up the helm release and k8s namespace
-if [ $ret -eq 0 ]; then
-    echo "Test completed successfully, deleting namespace and chart"
-    helm delete --namespace ${test_ns} wq2dss
-    kubectl delete ns "${test_ns}"
-fi
-echo "going to exit with ${ret}"
-exit ${ret}
+# run the tests in the cluster
+./dss/scripts/run_tests_in_cluster.sh $TAG
---
+++ dss/scripts/run_tests_in_cluster.sh
@@ -0,0 +1,32 @@
+#! /bin/bash -e
+# create a namespace for the test
+tag=$1
+test_ns=wq2dss-test-${tag}
+kubectl create ns ${test_ns}
+
+# deploy the helm chart with the testing flag set
+helm install wq2dss dss/chart/wq2dss/ --namespace ${test_ns} --set test.enabled=true
+
+# wait for everything to come up (no init container and readiness checks in place yet)
+# TODO: remove the sleep when waiting for all components will work
+sleep 30
+
+# hack for bash in windows
+if [ -n "$MSYSTEM" ] ; then
+    MSYS2_ARG_CONV_EXCL="/test"
+    echo "setting an exception for /test renaming"
+    export MSYS2_ARG_CONV_EXCL
+fi
+
+ret=0
+kubectl --namespace ${test_ns} exec -it wq2dss-test /test/run_tests.sh /test/ || ret=$?
+echo "Return value for test was $ret"
+
+# if test was successfull - clean up the helm release and k8s namespace
+if [ $ret -eq 0 ]; then
+    echo "Test completed successfully, deleting namespace and chart"
+    helm delete --namespace ${test_ns} wq2dss
+    kubectl delete ns "${test_ns}"
+fi
+echo "going to exit with ${ret}"
+exit ${ret}
