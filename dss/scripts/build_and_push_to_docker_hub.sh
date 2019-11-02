9e1cb2d9409164027560394d04e505e4fac59fac
Booh <ben.dayan@gmail.com>
2019-11-02 02:38:42 +0200

    Script updated to run tests in k8s via helm chart

--- dss/scripts/build_and_push_to_docker_hub.sh
+++ dss/scripts/build_and_push_to_docker_hub.sh
@@ -10,7 +10,9 @@ fi
 current_commit=$(git rev-parse --short HEAD)
 image_tag=${TAG:-$current_commit}
 docker build --rm -f "Dockerfile" -t waterqualitydss:latest -t "booooh/waterqualitydss:$image_tag" .
+docker build --rm -f "Dockerfile" -t waterqualitydss-test:latest -t "booooh/waterqualitydss-test:$image_tag" --target test .
 docker push "booooh/waterqualitydss:$image_tag"
+docker push "booooh/waterqualitydss-test:$image_tag"
 
 # update Chart.yaml
 if  [ -n "$bump_type" ] ; then
--- dss/scripts/build_and_run_tests.sh
+++ dss/scripts/build_and_run_tests.sh
@@ -1,5 +1,38 @@
 #! /bin/bash -e
-docker build --rm -f "Dockerfile" --build-arg http_proxy="$http_proxy" --build-arg https_proxy="$https_proxy" -t waterqualitydss-test:latest --target test .
-docker build --rm -f "Dockerfile" --build-arg http_proxy="$http_proxy" --build-arg https_proxy="$https_proxy" -t waterqualitydss:latest .
 
-docker-compose -f dss/test/docker-compose.yml up --abort-on-container-exit
+TAG=$(date +%s)
+export TAG
+
+# build the docker images, including the test image, and bump the appVersion in the chart
+./dss/scripts/build_and_push_to_docker_hub.sh bump
+
+# create a namespace for the test
+test_ns=wq2dss-test-${TAG}
+kubectl create ns ${test_ns}
+
+# deploy the helm chart with the testing flag set
+helm install wq2dss dss/chart/wq2dss/ --namespace ${test_ns} --set test.enabled=true
+
+# wait for everything to come up (no init container and readiness checks in place yet)
+# TODO: remove the sleep when waiting for all components will work
+sleep 30
+
+# hack for bash in windows
+if [ -n "$MSYSTEM" ] ; then
+    MSYS2_ARG_CONV_EXCL="/test"
+    echo "setting an exception for /test renaming"
+    export MSYS2_ARG_CONV_EXCL
+fi
+
+ret=0
+kubectl --namespace ${test_ns} exec -it wq2dss-test /test/run_tests.sh /test/ || ret=$?
+echo "Return value for test was $ret"
+
+# if test was successfull - clean up the helm release and k8s namespace
+if [ $ret -eq 0 ]; then
+    echo "Test completed successfully, deleting namespace and chart"
+    helm delete --namespace ${test_ns} wq2dss
+    kubectl delete ns "${test_ns}"
+fi
+echo "going to exit with ${ret}"
+exit ${ret}
