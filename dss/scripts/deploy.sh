6c6827321dd3d52a47b7367fe580b6aa233a7162
Booh <ben.dayan@gmail.com>
2019-11-26 11:08:40 +0200

    Change to evaluna for default deploy

--- dss/scripts/deploy.sh
+++ dss/scripts/deploy.sh
@@ -3,5 +3,7 @@
 TAG=$1
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push "$DOCKER_USERNAME/waterqualitydss:$TAG"
-docker push "$DOCKER_USERNAME/waterqualitydss-test:$TAG"
+
+docker_username=${DOCKER_USERNAME_FOR_REPO:-evaluna}
+docker push "${docker_username}/waterqualitydss:$TAG"
+docker push "${docker_username}/waterqualitydss-test:$TAG"
